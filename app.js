575134ecd5ad11c62ca9f1e27c82ae8567e47af8
Andres-Sq <asu0090@gmail.com>
2025-04-18 12:11:36 -0600

    clean code

--- app.js
+++ app.js
@@ -4,9 +4,3 @@ import smsRoutes from './routes/smsRoutes.js';
 
 app.use(express.json());
 app.use("/api/sms",smsRoutes);
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
--- controllers/smsController.js
+++ controllers/smsController.js
@@ -4,10 +4,8 @@ export function createSMS (req, res) {
     if (!bank || !phone_number || !amount || !details) 
         return res.status(400).json({ message: 'All fields are required' });
 
-    // Validate SMS data
     const message = `Pase ${amount} ${phone_number} ${details}`;
 
-    //Return the SMS data
     return res.status(200).json({
         bank,
         message
