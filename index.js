2c44f17252d81c1ab8ddd0ab197a0a625b4ef863
Andres-Sq <asu0090@gmail.com>
2025-05-04 16:44:35 -0600

    Revert "Add branch logs"
    This reverts commit d7073ee98730c701b48efa59ac188b6988801c7d, reversing
    changes made to 3f86e9c70ecb2decc40e2ccae9bd33e1690d7b06.

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import fs from 'fs';
-import path from 'path';
+const app = express();
 import smsRoutes from './routes/smsRoutes.js';
 import configRoutes from './routes/config.js';
-import logRoutes from './routes/logRoutes.js';
 
-const app = express();
-
-// --- Configurar CORS ---
+//app.use(cors());
 const allowedOrigins = [
     'https://sinpeapp.pages.dev', // Frontend en producción
   ];
@@ -24,30 +20,8 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
-// --- Middleware de logging ---
-app.use((req, res, next) => {
-  try {
-    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    const url = new URL(fullUrl);
-    const client = url.searchParams.get('client') || 'unknown';
-
-    const log = `[${new Date().toISOString()}] Client: ${client} | ${req.method} ${req.originalUrl} | IP: ${req.ip}\n`;
-
-    fs.appendFile(path.join('logs', 'transactions.log'), log, err => {
-      if (err) console.error('Error al escribir log:', err);
-    });
-  } catch (error) {
-    console.error('Error en middleware de log:', error);
-  }
-
-  next();
-});
-
-// --- Rutas ---
 app.use("/api/sms",smsRoutes);
 app.use('/api/config', configRoutes);
-app.use('/api/logs', logRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
